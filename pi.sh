0ad2895f64011626ada598406d7ab6805870f8eb
Arden Rasmussen <ardenisthebest@gmail.com>
2019-03-13 21:43:06 -0700

    Inital commit

--- pi.sh
+++ pi.sh
@@ -2,21 +2,22 @@
 
 START=1
 MAX=224399
-STEP=100
+STEP=500
 
-if [[ -z "$1" ]]; then
+if ! [[ -z "$1" ]]; then
     START=$1
 fi
-if [[ -z "$2" ]]; then
+if ! [[ -z "$2" ]]; then
     MAX=$2
 fi
-if [[ -z "$3" ]]; then
+if ! [[ -z "$3" ]]; then
     STEP=$3
 fi
 
 for i in $(seq $START $STEP $MAX); do
-  ./pi.py $i > pi.txt
-  git add -A
-  git commit -m"$(($i * 14))"
-  git push
+    printf "$i:$((i*14))\n"
+    ./pi.py $i > pi.txt
+    git add -A
+    git commit -m"$(($i * 14))"
+    git push
 done
