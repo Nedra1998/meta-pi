7aa00efb16bed9b2068a9b5be6f7b7e950d7763a
Arden Rasmussen <ardenisthebest@gmail.com>
2019-03-14 10:35:48 -0700

    Fixed file output

--- pi.py
+++ pi.py
@@ -7,7 +7,7 @@ def main():
     k = 100
     if sys.argv:
         k = int(sys.argv[1])
-    getcontext().prec = (15*k) + 10
+    getcontext().prec = (14*k) + 20
     K, M, L, X, S = 6, Decimal(1), Decimal(13591409), 1, 13591409
     for it in range(1,k+1):
         M = (K**3-(K<<4)) * M / k**3
@@ -16,8 +16,9 @@ def main():
         S += Decimal(M * L) / X
         K += 12
     pi = 426880 * Decimal(10005).sqrt() / S
-    pi = Decimal(str(pi)[:15*k])
-    print(pi)
+    pi = str(pi)[:14*k]
+    n = 80
+    print('\n'.join([pi[i:i+n] for i in range(0, len(pi), n)]))
 
 
 if __name__ == "__main__":
